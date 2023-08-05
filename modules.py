cfe1dfe832a9360ce1431badf4bc3f7ec673e3a0
Alireza Karimi <Alireza.Karimi.Programmer@gmail.com>
2023-08-05 20:37:39 +0330

    Create modules.py
    make a major part of modules.

---
+++ modules.py
@@ -0,0 +1,27 @@
+class Math:
+    
+    Symbols = ""
+    
+    Fnumber = 0
+    
+    Snumber = 0
+    
+    def __init__(self, Symbols, Fnumber, Snumber):
+        
+        return
+    
+    def Addition(self):
+        
+        return
+    
+    def Minus(self):
+        
+        return
+
+    def Multipliction(self):
+        
+        return
+
+    def Division(self):
+        
+        return
