e51a164f736d3a6d4a542141bf84583961a5518c
Alireza Karimi <Alireza.Karimi.Programmer@gmail.com>
2023-08-05 20:53:40 +0330

    Guideline and connect modules
    Write a simple greeting and Guideline.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Wellcome Console app Calculator.")
+from modules import Math
+
+print("""
+Greeting
+
+Hello there, Welcome to console app Calculator,i know this is a useless app 
+but i made it for my self to learn deeply OOP 'Object Oriented Programming'
+in below i wrote a Guide note for for you, i describe this for you.
+
+    *ATTENTION* first of all in all option you should to enter numbers and after you can enter your math symbols
+    
+    1. first option is for Addition, Enter symbols '+' in symbols input.
+""")
 
